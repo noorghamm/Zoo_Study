0d9ae5f5b19197f1b33092471f557c69211d0d32
Minghao Dai <3075295D@student.gla.ac.uk>
2026-03-06 20:47:47 +0000

    feat: enhance admin panel with search, filters, and list displays

--- zoo_app/admin.py
+++ zoo_app/admin.py
@@ -2,9 +2,45 @@ from django.contrib import admin
 from zoo_app.models import UserProfile, Task, StudySession, Animal, UserZoo, Resource
 
 
-admin.site.register(UserProfile)
-admin.site.register(Task)
-admin.site.register(StudySession)
-admin.site.register(Animal)
-admin.site.register(UserZoo)
-admin.site.register(Resource)
+@admin.register(UserProfile)
+class UserProfileAdmin(admin.ModelAdmin):
+    list_display = ('user', 'currency', 'streak', 'last_study_date', 'zoo_animal_count')
+    search_fields = ('user__username',)
+    list_filter = ('streak',)
+
+
+@admin.register(Task)
+class TaskAdmin(admin.ModelAdmin):
+    list_display = ('title', 'user', 'deadline', 'completed', 'is_overdue')
+    search_fields = ('title', 'user__username')
+    list_filter = ('completed', 'deadline')
+    list_editable = ('completed',)
+
+
+@admin.register(StudySession)
+class StudySessionAdmin(admin.ModelAdmin):
+    list_display = ('user', 'duration_minutes', 'coins_earned', 'date')
+    search_fields = ('user__username',)
+    list_filter = ('date',)
+
+
+@admin.register(Animal)
+class AnimalAdmin(admin.ModelAdmin):
+    list_display = ('name', 'category', 'cost', 'study_hours_required')
+    search_fields = ('name',)
+    list_filter = ('category',)
+    prepopulated_fields = {'slug': ('name',)}
+
+
+@admin.register(UserZoo)
+class UserZooAdmin(admin.ModelAdmin):
+    list_display = ('user', 'animal', 'date_acquired')
+    search_fields = ('user__username', 'animal__name')
+    list_filter = ('animal__category', 'date_acquired')
+
+
+@admin.register(Resource)
+class ResourceAdmin(admin.ModelAdmin):
+    list_display = ('title', 'task', 'type', 'created_date')
+    search_fields = ('title', 'task__title')
+    list_filter = ('type', 'created_date')
